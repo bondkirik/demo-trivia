6a0312beec62943fc0e40e995740466fa949601b
Kirils Bondarevs <bondarevs@co.inbox.lv>
2021-03-10 15:31:04 +0200

    add api server, get json output

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,48 @@
-console.log(1);
+window.onload = sendApiRequest;
+
+const btn = document.getElementById("btn");
+const result = document.querySelector('#result');
+const question = document.querySelector('#question');
+
+const server = `http://numbersapi.com/random/trivia?json&fragment&min=1&max=9999`;
+
+
+/**
+ * api parser
+ * @param event
+ * @returns {Promise<void>}
+ */
+
+async function sendApiRequest(event) {
+    event.preventDefault();
+    await fetch(server).then(function (value) {
+        if(value.status !== 200){
+            return Promise.reject(new Error(value.status));
+        }
+        return value.json();
+
+    }).then(function (output) {
+            let inner = '';
+
+            if (!output.found) {
+                inner = '<h2 class="option wrong">Nothing found</h2>';
+            }
+
+                inner += `<div class="option">${output.number}</div>`;
+                inner += `<div class="option">1</div>`;
+                inner += `<div class="option">2</div>`;
+
+
+            question.innerHTML = output.text;
+            result.innerHTML = inner;
+
+
+
+    })
+        .catch(function (reason) {
+            result.innerHTML = 'Something wrong';
+            console.error('error' + reason);
+        });
+}
+
+btn.addEventListener("click", sendApiRequest);
