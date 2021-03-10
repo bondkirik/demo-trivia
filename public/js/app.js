f4a5a76c38893784531ddb2a622d5459cc5d2130
Kirils Bondarevs <bondarevs@co.inbox.lv>
2021-03-10 15:50:25 +0200

    checking a repeat question

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,17 @@ window.onload = sendApiRequest;
 const btn = document.getElementById("btn");
 const result = document.querySelector('#result');
 const question = document.querySelector('#question');
+var q_number = document.getElementById("q_number");
 
-const server = `http://numbersapi.com/random/trivia?json&fragment&min=1&max=9999`;
+const MAX_QUESTIONS = 20;
+const MAX = 9999;
+const answers_check = [];
+
+const server = `http://numbersapi.com/random/trivia?json&fragment&min=1&max=`+MAX;
 
 
 /**
- * api parser
+ * api server
  * @param event
  * @returns {Promise<void>}
  */
@@ -21,23 +26,36 @@ async function sendApiRequest(event) {
         }
         return value.json();
 
+    }).then(function (data) {
+
+        answers_check.forEach(answer => {
+            if (answer === data.number) {
+                data = false;
+            }
+        });
+        return data;
+
     }).then(function (output) {
+        if (!output){
+            sendApiRequest(event);
+        }else {
             let inner = '';
 
             if (!output.found) {
                 inner = '<h2 class="option wrong">Nothing found</h2>';
             }
 
-                inner += `<div class="option">${output.number}</div>`;
-                inner += `<div class="option">1</div>`;
-                inner += `<div class="option">2</div>`;
+            inner += `<div class="option">${output.number}</div>`;
+            inner += `<div class="option">1</div>`;
+            inner += `<div class="option">2</div>`;
 
 
             question.innerHTML = output.text;
             result.innerHTML = inner;
 
+            answers_check.push(output.number);
 
-
+        }
     })
         .catch(function (reason) {
             result.innerHTML = 'Something wrong';
